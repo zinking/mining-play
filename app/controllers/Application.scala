e53ea06b47a7ecfa7d3926883c73964833783bb8
zinking <zinking3@gmail.com>
2014-03-23 00:17:42 +0800

    not working yet.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,11 +4,15 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller{
-  def index = Action {
+  def index = Action { implicit request =>
     Ok("Hello World")
   }
   
   def login = Action {
     Ok("Login Here")
+  }
+  
+  def default = Action{ request =>
+    NotImplemented
   }
 }
---
+++ app/controllers/User.scala
@@ -0,0 +1,76 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object User extends Controller{
+  
+  def addSubscription = Action{ request =>
+    NotImplemented
+  }
+  
+  def deleteAccount = Action{ request =>
+    NotImplemented
+  }
+  
+  def exportOPML = Action{ request =>
+    NotImplemented
+  }
+  
+  def feedHistory = Action{ request =>
+    NotImplemented
+  }
+  
+  def getContents = Action{ request =>
+    NotImplemented
+  }
+  
+  def getFee = Action{ request =>
+    NotImplemented
+  }
+  
+  def getStars = Action{ request =>
+    NotImplemented
+  }
+  
+  def importOPML = Action{ request =>
+    NotImplemented
+  }
+  
+  def listFeeds = Action{ request =>
+    NotImplemented
+  }
+  
+  def markRead = Action{ request =>
+    NotImplemented
+  }
+  
+  def markUnread = Action{ request =>
+    NotImplemented
+  }  
+  
+  def saveOptions = Action{ request =>
+    NotImplemented
+  }
+  
+  def setStar = Action{ request =>
+    NotImplemented
+  }
+  
+  def uploadOPML = Action{ request =>
+    NotImplemented
+  }
+  
+  def charge = Action{ request =>
+    NotImplemented
+  }
+  
+  def account = Action{ request =>
+    NotImplemented
+  }
+  
+  def unCheckout = Action{ request =>
+    NotImplemented
+  }    
+  
+}
