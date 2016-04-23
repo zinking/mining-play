97c1a08d00c74bab8f85fee0e7573f33506312b9
zhen wang <zinking3@gmail.com>
2016-04-23 23:30:19 +0800

    fix concurrent refresh feed issue

--- app/actors/FeedRefreshManager.scala
+++ app/actors/FeedRefreshManager.scala
@@ -1,26 +1,28 @@
 package actors
 
 import java.util.Date
+import java.util.concurrent.ConcurrentHashMap
 
 import akka.actor.{ActorLogging, Actor, Props}
 import com.typesafe.config.{ConfigFactory, Config}
 import mining.io.Feed
 import mining.io.dao.FeedDao
+import scala.collection.concurrent.Map
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.concurrent.{ExecutionContext, Future}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scala.collection.JavaConverters._
+import scala.language.reflectiveCalls
 
 /**
- * Feed refresh mananger
+ * Feed refresh manager
  *
  * Created by awang on 30/8/15.
  */
 
-case class RefreshFirstQuarterFeeds()
-case class RefreshSecondQuarterFeeds()
-case class RefreshThirdQuarterFeeds()
-case class RefreshAllFeeds()
+
+case class RefreshFeeds(index:Int, duration:Int)
 
 object FeedRefreshManager {
     def props = Props[FeedRefreshManager]
@@ -30,24 +32,26 @@ object FeedRefreshManager {
     val feedChgInt = conf.getConfig(env).getInt("feedchg-int")
     val refreshInt = conf.getConfig(env).getInt("refresh-int")
     
-    val d1 = feedChgInt
-    val d2 = feedChgInt*4
-    val d3 = feedChgInt*16
-    val d4 = feedChgInt*64
+    val d1 = refreshInt
+    val d2 = d1*4
+    val d3 = d1*16
+    val d4 = d1*64
 
-    val refreshFirstBatch = RefreshFirstQuarterFeeds()
-    val refreshSecondBatch = RefreshSecondQuarterFeeds()
-    val refreshThirdBatch = RefreshThirdQuarterFeeds()
-    val refreshAllMsg = RefreshAllFeeds()
+    val refresh1 = RefreshFeeds(1, d1)
+    val refresh2 = RefreshFeeds(2, d2)
+    val refresh3 = RefreshFeeds(3, d3)
+    val refresh4 = RefreshFeeds(4, d4)
 }
 
 class FeedRefreshManager extends Actor with ActorLogging {
     import FeedRefreshManager._ 
     val feedDAO = FeedDao()
-    context.system.scheduler.schedule(d1 seconds, d1 seconds, self, refreshFirstBatch)
-    context.system.scheduler.schedule(d2 seconds, d2 seconds, self, refreshSecondBatch)
-    context.system.scheduler.schedule(d3 seconds, d3 seconds, self, refreshThirdBatch)
-    context.system.scheduler.schedule(d4 seconds, d4 seconds, self, refreshAllMsg)
+    context.system.scheduler.schedule(d1 seconds, d1 seconds, self, refresh1)
+    context.system.scheduler.schedule(d2 seconds, d2 seconds, self, refresh2)
+    context.system.scheduler.schedule(d3 seconds, d3 seconds, self, refresh3)
+    context.system.scheduler.schedule(d4 seconds, d4 seconds, self, refresh4)
+
+    val feedLock: Map[String, String] = new ConcurrentHashMap[String,String]().asScala
 
 
     def getQuarterFeeds( index:Int, duration:Int):Iterable[Feed] = {
@@ -71,30 +75,25 @@ class FeedRefreshManager extends Actor with ActorLogging {
         }
     }
 
-
     def receive = {
-        case msg:RefreshFirstQuarterFeeds =>
-            getQuarterFeeds(1,d1).foreach({feed=>
-                log.info("Refresh feed {}",feed.xmlUrl)
-                feedDAO.createOrUpdateFeed(feed.xmlUrl)
+        case RefreshFeeds(id, duration) =>
+            val banner = "***********************************************************************"
+            log.info(" BEGIN {} {} {}", id, duration, banner)
+            getQuarterFeeds(id,duration).foreach({feed=>
+                val feedUrl = feed.xmlUrl
+                feedLock.get(feedUrl) match {
+                    case Some(xml) =>
+                        log.info("Skip locked feed quarter {} duration {} url {}", id, duration, feedUrl)
+                    case None =>
+                        feedLock.put(feedUrl, "1")
+                        log.info("Refresh feed quarter {} duration {} url {}", id, duration, feedUrl)
+                        feedDAO.createOrUpdateFeed(feed.xmlUrl) onComplete {
+                            case _ => feedLock.remove(feedUrl)
+                        }
+                }
+
             })
 
-        case msg:RefreshSecondQuarterFeeds =>
-            getQuarterFeeds(2,d2).foreach({feed=>
-                log.info("Refresh feed {}",feed.xmlUrl)
-                feedDAO.createOrUpdateFeed(feed.xmlUrl)
-            })
-
-        case msg:RefreshThirdQuarterFeeds =>
-            getQuarterFeeds(3,d3).foreach({feed=>
-                log.info("Refresh feed {}",feed.xmlUrl)
-                feedDAO.createOrUpdateFeed(feed.xmlUrl)
-            })
-
-        case msg:RefreshAllFeeds =>
-            getQuarterFeeds(4,d4).foreach({feed=>
-                log.info("Refresh feed {}",feed.xmlUrl)
-                feedDAO.createOrUpdateFeed(feed.xmlUrl)
-            })
+            log.info(" ENDET {} {} {}", id, duration, banner)
     }
 }
